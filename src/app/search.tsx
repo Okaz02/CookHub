a3eb83d99e174caf6306b45c383f734af3bac643
Kazuya Yoshioka <kazuyayosioka@gmail.com>
2026-09-01 08:07:55 +0900

    feat: update Search component layout and add input box for search functionality

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,20 +1,56 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, TextInput, View, StyleSheet, ScrollView, Image } from "react-native";
 
 export default function Search() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>検索</Text>
+    <View style={styles.screen}>
+      <TextInput style={styles.inputBox} />
+      <ScrollView style={styles.scrollView} contentContainerStyle={styles.container}>
+        <View style={styles.recipe}>
+          <Image style={styles.recipeImage} />
+          <View style={styles.recipeExplain}>
+            <Text>ヴォルデモート</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
+    gap: 20
   },
-  text: {
-    fontSize: 20,
+  scrollView: {
+    flex: 1
   },
+  container: {
+    alignContent: "center",
+    gap: 20,
+  },
+  inputBox: {
+    textAlign: "center",
+    padding: 20,
+    height: 40,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#b1b1b1",
+    backgroundColor: "#dbdbdb"
+  },
+  recipe: {
+    backgroundColor: "black",
+    height: 350,
+    overflow: "hidden",
+    borderRadius: 20,
+  },
+  recipeImage: {
+    height: 200,
+  },
+  recipeExplain: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#dbdbdb"
+  }
 });
